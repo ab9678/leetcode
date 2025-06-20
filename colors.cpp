ae533fccd05d87fc2d2b349c12985a9b9a5b4dd9
Abhigyan <abhigyanbhattacharya75@gmail.com>
2025-06-20 12:13:32 +0530

    Update

---
+++ colors.cpp
@@ -0,0 +1,11 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+    int nums[6];
+
+    cout<<"Enter 0 1 2 for red white blue: "; 
+    for(int i=0; i<6; i++){
+        cin >> nums[i];
+    }
+}
---
+++ shortestSubstring.cpp
@@ -0,0 +1,29 @@
+# include <iostream>
+# include <string>
+using namespace std;
+
+
+class substring{
+
+    string ostring;
+    
+    void subtring(){
+
+
+
+
+    
+    }
+
+    public:
+    substring(){
+        cout << "Enter a string: ";
+        cin >> ostring;
+    }
+
+};
+
+
+int main(){
+
+}
